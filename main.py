50dbd2cf0d531bd2a8ee20d331a290db0a363d76
Steve <sntakirutimana72@gmail.com>
2023-04-05 23:38:11 +0200

    Update app :main entry file

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 if __name__ == '__main__':
     from src.helpers.logger import Logger
+    from src.protocols import (
+        configure_framework_logging_system, configure_system_source_for_compiled_version,
+        configure_app_window_on_startup,
+    )
 
+    configure_framework_logging_system()
+    configure_system_source_for_compiled_version()
     Logger.setup_logger()
     Logger.info('Starting application')
     Logger.info('Engage startup protocol')
-
-    from src.protocols.startup import routine_001
-
-    routine_001()
+    configure_app_window_on_startup()
     Logger.info('Startup protocol engaged successfully.')
 
-    from src.views.app import BluechClientApp
+    from src.app import BluechClientApp
 
-    BluechClientApp().run()
+    try:
+        BluechClientApp().run()
+    except:
+        Logger.error()
     Logger.info('Application closed')
