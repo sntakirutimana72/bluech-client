80079a89897785154d01ce09ad9edcb91275d5ef
Steve <sntakirutimana72@gmail.com>
2023-04-09 23:11:52 +0200

    Refactor app gui logic

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
 from kivy.app import App
+from kivy.properties import ObjectProperty
 
-from .workers import Worker
+from .helpers.workers import Worker
 from .views import Dashboard
 
 class BluechClientApp(App):
+    worker: Worker = ObjectProperty()
+
     def build(self):
-        return Dashboard(worker=Worker())
+        self.worker = Worker()
+        root_widget = Dashboard(app=self)
+
+        return root_widget
+
+    def on_start(self):
+        self.worker.ignite()
