ac936b6c44e65f2040a77d2ec7df929647699c10
Steve <sntakirutimana72@gmail.com>
2023-04-16 10:14:45 +0200

    Update app main logic and layout

--- src/app.py
+++ src/app.py
@@ -1,13 +1,20 @@
+from kivy.properties import ObjectProperty, DictProperty
 from kivy.app import App
-from kivy.properties import ObjectProperty
 
-from .helpers.workers import Worker
-from .views import Dashboard
+from .settings import COMMON_KV_TEMPLATES
+from .templates.views import Dashboard
+from .utils.directives import include
+from .utils.workers import Worker
 
 class BluechClientApp(App):
     worker: Worker = ObjectProperty()
+    user = DictProperty({
+        'is_authenticated': False,
+        'is_anonymous': True
+    })
 
     def build(self):
+        include(COMMON_KV_TEMPLATES)
         self.worker = Worker()
         root_widget = Dashboard(app=self)
 
@@ -15,3 +22,14 @@ class BluechClientApp(App):
 
     def on_start(self):
         self.worker.ignite()
+
+    def on_signed_in(self, **user):
+        user['is_authenticated'] = True
+        user['is_anonymous'] = False
+        self.user |= user
+
+    def on_signed_out(self):
+        self.user = {
+            'is_authenticated': False,
+            'is_anonymous': True
+        }
