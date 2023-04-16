7752398af890d85dca89666652c7e134253c9a5b
Steve <sntakirutimana72@gmail.com>
2023-04-16 10:10:27 +0200

    Extend & update directive logics

--- src/templates/shortcuts/directives.py
+++
@@ -1,7 +0,0 @@
-import pathlib as plib
-
-from kivy.lang import Builder
-
-def include(py_file_path: str):
-    kv_file_path = plib.Path(py_file_path).with_suffix('.kv')
-    Builder.load_file(str(kv_file_path))
