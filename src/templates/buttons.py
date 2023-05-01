ad99a7a022241dfdbc82960fd50b56fca257f7cd
Steve <sntakirutimana72@gmail.com>
2023-05-01 06:56:52 +0200

    Move certain design from /behaviors back in /templates

---
+++ src/templates/buttons.py
@@ -0,0 +1,13 @@
+from kivy.uix.label import Label
+from kivy.properties import ListProperty, StringProperty, ColorProperty
+
+from .behaviours import Clicking, Hovering
+from ..utils.directives import include
+
+include(__file__)
+
+class PrimaryButton(Clicking, Hovering, Label):
+    border_radius = ListProperty([4, 4, 4, 4])
+    background_color = ColorProperty('#40ff88ff')
+    disabled_color = ColorProperty('gray')
+    text = StringProperty('Click Me!')
