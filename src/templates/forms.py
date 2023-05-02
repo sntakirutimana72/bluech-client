5da40ff04d5a3d5f8684040b671c9b7d2e9f36f5
Steve <sntakirutimana72@gmail.com>
2023-05-02 03:23:59 +0200

    Create form error display component

--- src/templates/forms.py
+++ src/templates/forms.py
@@ -1,12 +1,16 @@
 from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.label import Label
 
 from .fields import TextField
 from .progress_elements import RingSpin
-from .layouts import Grid
+from .layouts import Grid, GUILayout
 from .buttons import PrimaryButton
 from ..utils.directives import include
 
 include(__file__)
+
+class FormErrorDisplay(GUILayout, Label):
+    ...
 
 class LogonForm(Grid):
     __events__ = 'on_submit',
@@ -14,6 +18,7 @@ class LogonForm(Grid):
     username: TextField = ObjectProperty()
     password: TextField = ObjectProperty()
     submit_btn: PrimaryButton = ObjectProperty()
+    errors = StringProperty('')
     title = StringProperty('[i][font=Comic]b[color=#0cf]lue[/color]ch[/font][/i] [b]Logon[/b]')
 
     def pre_submit(self):
@@ -27,9 +32,12 @@ class LogonForm(Grid):
     def post_submit(self, **kwargs):
         sibling = self.submit_btn.parent.children[0]
         ring_spin: RingSpin = sibling.children[0]
+        is_disabled = kwargs['is_disabled']
+        if is_disabled is False:
+            self.errors = kwargs['error']
         ring_spin.halt()
         sibling.remove_widget(ring_spin)
-        self.flag_form(kwargs['is_disabled'])
+        self.flag_form(is_disabled)
 
     def on_submit(self):
         """Triggered when the form is submitted."""
@@ -40,9 +48,19 @@ class LogonForm(Grid):
 
     def on_password_change(self, new_val: str):
         """Isolate live updates to :attr:`password`."""
-        self.flag_submit(self.password.value, new_val)
+        self.flag_submit(self.username.value, new_val)
+
+    def on_errors(self, *args):
+        errors = args[1]
+        container = self.ids.errors_container
+        if errors:
+            display = FormErrorDisplay(text=args[1])
+            container.add_widget(display)
+        else:
+            container.clear_widgets()
 
     def flag_submit(self, usr: str, usr_pass: str):
+        self.errors = ''
         self.submit_btn.disabled = not (usr and usr_pass)
 
     def flag_form(self, is_disable: bool):
