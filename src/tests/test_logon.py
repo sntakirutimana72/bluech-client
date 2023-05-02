9aa51042387c8dad3f252f42f63f1bcefc4158d6
Steve <sntakirutimana72@gmail.com>
2023-05-02 03:38:21 +0200

    Test simulated behavioral reflexes of GUI logic to server responses.

--- src/tests/test_logon.py
+++ src/tests/test_logon.py
@@ -1,39 +1,40 @@
 from .supports.unittest import GUnittest
 from .supports.blocks import async_exc
+from ..templates.forms import LogonForm
 
 class LogonTestCases(GUnittest):
     def build(self):
         super().build()
         index_page = self.current_page
         index_page.on_status(status='online')
-        with async_exc(self.find_by_role, self.current_page, 'LogonForm') as form:
-            self.assertIsNotNone(form)
-            self.form = form
+        form = getattr(self.current_page, 'form')
+        self.assertIsInstance(form, LogonForm)
+        self.form = form
 
-class FormContainsElements(LogonTestCases):
-    def test_form_contains_username_field(self):
-        self.build()
-        with async_exc(self.find_by_attrib, self.form, 'name', r'^username$') as element:
-            self.assertIsNotNone(element)
-            self.assertTrue(hasattr(element, 'value'))
-
-    def test_form_contains_password_field(self):
-        self.build()
-        with async_exc(self.find_by_attrib, self.form, 'name', r'^password$') as element:
-            self.assertIsNotNone(element)
-            self.assertTrue(hasattr(element, 'value'))
-
-    def test_form_contains_submit_button(self):
-        self.build()
-        with async_exc(self.find_by_role, self.form, 'PrimaryButton') as element:
-            self.assertIsNotNone(element)
-            self.assertIn(element.text, 'Signin')
-
-class SubmissionDisability(LogonTestCases):
     def enter_credentials(self, **kwargs):
         self.form.username.value = kwargs['username']
         self.form.password.value = kwargs['pass_w']
 
+class FormContainsElements(LogonTestCases):
+    def test_form_contains_username_field(self):
+        self.build()
+        username_field = self.form.username
+        self.assertIsNotNone(username_field)
+        self.assertTrue(hasattr(username_field, 'value'))
+
+    def test_form_contains_password_field(self):
+        self.build()
+        password_field = self.form.password
+        self.assertIsNotNone(password_field)
+        self.assertTrue(hasattr(password_field, 'value'))
+
+    def test_form_contains_submit_button(self):
+        self.build()
+        submit_btn = self.form.submit_btn
+        self.assertIsNotNone(submit_btn)
+        self.assertIn(submit_btn.text, 'Signin')
+
+class SubmissionDisability(LogonTestCases):
     def test_disability_by_default(self):
         self.build()
         self.assertFalse(self.form.username.disabled)
@@ -53,7 +54,41 @@ class SubmissionDisability(LogonTestCases):
     def test_disability_on_submission(self):
         self.build()
         self.enter_credentials(username='steve@email', pass_w='pass@123')
-        self.form.submit_btn.dispatch('on_press')
+        self.click(self.form.submit_btn)
+
         self.assertTrue(self.form.submit_btn.disabled)
         self.assertTrue(self.form.username.disabled)
         self.assertTrue(self.form.password.disabled)
+
+class PostSubmissionBehaviors(LogonTestCases):
+    def pre_signin(self):
+        self.build()
+        self.enter_credentials(username='stevie', pass_w='@123')
+
+        self.assertFalse(self.form.submit_btn.disabled)
+        self.click(self.form.submit_btn)
+        self.assertTrue(self.form.submit_btn.disabled)
+
+    def test_failure_response(self):
+        self.pre_signin()
+        response = {'proto': 'invalid_request', 'message': '400: Invalid Request'}
+        self.current_page.on_signed_in(**response)
+        self.assertFalse(self.form.submit_btn.disabled)
+        self.assertEqual(self.form.errors, response['message'])
+        self.assertEqual(len(self.form.ids.errors_container.children), 1)
+
+    def test_success_response(self):
+        self.pre_signin()
+        response = {
+            'proto': 'connected',
+            'user': {
+                'email': 'user@email',
+                'nickname': 'user@nickname',
+                'id': 7089
+            }
+        }
+        logon_page = self.current_page
+        logon_page.on_signed_in(**response)
+        self.assertTrue(self.form.submit_btn.disabled)
+        self.assertNotEqual(self.current_page, logon_page)
+        self.assertFalse(self.current_page.app.user['is_anonymous'])
